28881c163973df1b2174bf1964ddf44d575a7c19
CristobalHunto <76532078+CristobalHunto@users.noreply.github.com>
2025-04-11 02:42:42 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class CBase
+{
+private:
+    int i;
+
+public:
+    CBase() {this->i = 0;}
+    CBase(int j) {this->i = j;};
+    int get() { return this->i;}
+    void add(int j) {this->i += j;};
+};
+
+class CLast: CBase
+{
+public:
+    CLast(int j) {this->add(j);};
+    int get() {return -1;}
+    void print(){std::cout << "i = " << this->get() << std::endl;};
+    void printB(){std::cout << "i = " << CBase::get() << std::endl;};
+};
+
+
+int main()
+{
+    CLast a(1);
+    
+    a.print();
+    a.printB();
+
+    return 0;
+}
